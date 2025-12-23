f2a48e749bbf43898f86d8b7b7eb91ffd342ae61
Appasab <appayyakambale66@gmail.com>
2025-12-23 22:21:55 +0530

    feat: sprint-1 day-2 prisma setup and initial database schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const prisma = require("./config/prisma");
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("School Management API running");
+});
+
+app.get("/test-db", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 module.exports = app;
